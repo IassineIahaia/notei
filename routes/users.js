d55dcfeff98968b13f9f88b57362919bafdcd4b6
Iassine Iahaia <zinhocabila58@gmail.com>
2025-06-05 14:36:25 +0200

    feito autenticação do login do usuario

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const exepress = require('express');
 const router = exepress.Router();
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secret = process.env.JWT_TOKEN;
 
 router.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
@@ -10,6 +13,30 @@ router.post('/register', async (req, res) => {
         res.status(201).json(user);
     } catch (error) {
         res.status(400).json({ error: error.message });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Usuário não encontrado' });
+        }
+
+        const isPasswordValid = await user.comparePassword(password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Senha inválida' });
+        }
+
+        const token = jwt.sign({ id: user._id }, secret, { expiresIn: '30d' });
+
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 });
 
